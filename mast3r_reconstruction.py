6fa61caf220e09c5e4d753b4ce2618e1d63c0b80
lucacicchese <luca.cicchese@gmail.com>
2025-11-02 15:44:19 +0100

    fix

--- mast3r_reconstruction.py
+++ mast3r_reconstruction.py
@@ -5,7 +5,7 @@ import subprocess
 import shutil
 import random
 import warnings
-from utils import split_large_image_folder
+from utils import split_dataset
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
